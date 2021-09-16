13ca3d7d2f0b0fd0ae3ad5bb6ed1c3f4fc7bfef8
Styshooteur <70535416+Styshooteur@users.noreply.github.com>
2021-09-16 11:37:08 +0200

    Initial commit

---
+++ include/engine.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <system.h>
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+
+class Engine
+{
+public:
+    Engine();
+    void AddSystem(System* system);
+    void AddDrawSystem(DrawInterface* drawSystem);
+    void AddDrawImGuiSystem(DrawImGuiInterface* drawImGuiSystem);
+    void Run();
+private:
+    void Init();
+    void Update(sf::Time dt);
+    void Destroy();
+
+    std::vector<System*> systems_;
+    std::vector<DrawInterface*> drawSystems_;
+    std::vector<DrawImGuiInterface*> drawImGuiSystems_;
+    sf::RenderWindow window_; 
+};
